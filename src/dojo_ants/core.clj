84b1ea2d6e113770c776c5ee8de7a18b6bb2511c
Bruce Durling <bld@cassini.(none)>
2011-03-29 18:41:43 +0100

    Initial commit.

---
+++ src/dojo_ants/core.clj
@@ -0,0 +1,1 @@
+(ns dojo-ants.core)
---
+++ test/dojo_ants/test/core.clj
@@ -0,0 +1,6 @@
+(ns dojo-ants.test.core
+  (:use [dojo-ants.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
